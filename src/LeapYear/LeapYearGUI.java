50ac4a8688811ae25f04f0b231b97286c61e821d
Winson Caburnay Baring <winsonbaring10@gmail.com>
2022-12-17 10:36:35 +0800

    Renamed LeapYear Component

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,13 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+    private JLabel Jlabel;
+
 }
