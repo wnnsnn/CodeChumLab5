491dead919e4289a7a61113bfe2e09ab225b72b8
Winson Caburnay Baring <winsonbaring10@gmail.com>
2022-12-17 13:34:40 +0800

    Final FoodOrderingGUI

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -20,5 +20,76 @@ public class FoodOrderGUI extends JFrame{
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    List<JRadioButton> Discount;
+    List<JCheckBox> Menu;
+    public FoodOrderGUI(){
+        Menu = new ArrayList<>();
+        Menu.add(cBurger);
+        Menu.add(cPizza);
+        Menu.add(cFries);
+        Menu.add(cSoftDrinks);
+        Menu.add(cTea);
+        Menu.add(cSundae);
+        Discount = new ArrayList<>();
+        Discount.add(rbNone);
+        Discount.add(rb5);
+        Discount.add(rb10);
+        Discount.add(rb15);
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Buy();
+                }catch(Exception ee){
+                    JOptionPane.showMessageDialog(panel1, ee.getMessage());
+                }
+            }
+        });
+    }
+    public static void main(String[] args) {
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setContentPane(app.panel1);
+        app.setTitle("Food Ordering System");
+        app.setSize(500,700);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 
+    public void Buy(){
+        double total = 0.0;
+        for(JCheckBox mo: Menu){
+            if(mo.isSelected()) {
+                if (mo == cPizza) {
+                    total = total + 100;
+                }
+                if (mo == cBurger) {
+                    total = total + 80;
+                }
+                if (mo == cFries) {
+                    total = total + 65;
+                }
+                if (mo == cSoftDrinks) {
+                    total = total + 55;
+                }
+                if (mo == cTea) {
+                    total = total + 50;
+                }
+                if (mo == cSundae) {
+                    total = total + 40;
+                }
+            }
+        }
+        for(JRadioButton rb: Discount){
+            if(rb.isSelected()) {
+                if (rb == rb5) {
+                    total = total - (total * .05);
+                }if( rb == rb10){
+                    total = total - (total * .1);
+                }if(rb == rb15){
+                    total = total-(total * .15);
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(panel1, String.format("The total price is Php %.2f", total));
+    }
 }
